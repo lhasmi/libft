af0f13600abbce1965e14380dfd1a7a75cb3f5b7
Laila H <lailah@Lailas-MacBook-Pro.local>
2022-05-21 20:05:40 +0200

    memchrfixed

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,38 +6,32 @@
 /*   By: lailah <lailah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 17:53:21 by lhasmi            #+#    #+#             */
-/*   Updated: 2022/05/21 17:49:05 by lailah           ###   ########.fr       */
+/*   Updated: 2022/05/21 20:05:10 by lailah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*locates the first occurrence of c (converted to an unsigned char) in string s.
-
-RETURN VALUES
-     The memchr() function returns a pointer to the byte located,
-	 or NULL
-     if no such byte exists within n bytes.
-
-	 1- check if s is null
-	 2- type casting c to unsigned character
-	 3- go trough the s and if s[i] == c
-	 4- return the pointer s+i
-*/
 #include"libft.h"
-
+#include<stdio.h>
 void * ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
 	char	*str;
 
 	str = (char	*)s;
+	if (n == 0)
+		return(NULL);
 	i = 0;
-	while(str[i] && i < n)
+	printf("str[%zu] = %s\n", i, &str[i]);
+	while(i < n)
 	{
-		if (str[i] == (char )c)
-			return (&str[i]);
+		printf("entering the loop\n");
+		if (str [i] == (unsigned char)c){
+			printf("after c is found\n");
+			return (str + i);
+		}
 		i++;
 	}
 	if (c == '\0')
-        return (&str[i]); //char *ptr4 = &a[6];  // Points to the 'w' of "world"
-    return (0);
+		return (str + i); 
+	return (0);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lailah <lailah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 17:52:07 by lhasmi            #+#    #+#             */
-/*   Updated: 2022/05/21 17:44:12 by lailah           ###   ########.fr       */
+/*   Updated: 2022/05/21 19:52:01 by lailah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,110 +46,3 @@ char	*ft_strnstr(const char * haystack, const char * needle, size_t len)
     }
     return (NULL);
 }
-
-
-/*#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-    const char    *str0;
-    const char    *str1;
-    const char    *str2;
-    // char    *str3;
-    // char    *str4;
-    // char    *str5;
-    // char    *str6;
-    // char    *str7;
-    // char    *str8;
-    // char    *str9;
-    str0 = ft_strnstr("lorem ipsum dolor sit amet", "lorem", 15);
-    str1 = ft_strnstr("lorem ipsum dolor sit amet", "ipsum", 15);
-    str2 = ft_strnstr("lorem ipsum dolor sit lorem ipsum dolor", "ipsum", 35);
-    // str3 = ft_strnstr("lorem ipsum dolor sit amet", "", 10);
-    // str4 = ft_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30);
-    // str5 = ft_strnstr("lorem ipsum dolor sit amet", "dol", 30);
-    // str6 = ft_strnstr("lorem ipsum dolor sit amet", "consectetur", 30);
-    // str7 = ft_strnstr("lorem ipsum dolor sit amet", "sit", 10);
-    // str8 = ft_strnstr("lorem ipsum dolor sit amet", "dolor", 15);
-    // str9 = ft_strnstr("lorem ipsum dolor sit amet", "dolor", 0);
-
-// const char	hay[100] = "lorem ipsum dolor sit amet";
-// const char	hay1[100]= "lorem ipsum dolor sit amet";
-// const char	nee[100]= "HZ";
-// const char	nee1[110]= "HZ";
-// size_t	len = strlen(nee);
-// size_t	len1 = strlen(nee1);
-// size_t	len = strlen(hay);
-// size_t	len1 = strlen(hay1);
-// size_t	len = '2';
-// size_t	len1 = '3';
-// size_t	len = strlen(nee);
-// size_t	len = strlen(nee);
-// size_t	len = strlen(nee);
-// size_t	len = strlen(nee);
-// size_t	len = strlen(nee);
-// size_t	len = strlen(nee);
-    printf("pointer to nee trough ft_strnstr: %p\n", str0);
-    printf("pointer to nee trough strnstr : %p\n", str0);
-    printf("pointer to nee trough ft_strnstr : %p\n", str1);
-    printf("pointer to nee trough strnstr : %p\n", str1);
-    printf("pointer to nee trough ft_strnstr : %p\n", str2);
-    printf("pointer to nee trough strnstr : %p\n", str2);
-    // printf("pointer to nee trough strnstr : %p, the needle is:  %s\n", str3, str3 );
-    // printf("pointer to nee trough strnstr : %p, the needle is:  %s\n", str4, str4 );
-    // printf("pointer to nee trough strnstr : %p, the needle is:  %s\n", str5, str5);
-    // printf("pointer to nee trough strnstr : %p, the needle is:  %s\n", str6, str6);
-    // printf("pointer to nee trough strnstr : %p, the needle is:  %s\n", str7, str7);
-    // printf("pointer to nee trough strnstr : %p, the needle is:  %s\n", str8, str8);
-    // printf("pointer to nee trough strnstr : %p, the needle is:  %s\n", str9, str9);
-}*/
-
-/*
-Strnstr
-
-Localise la premiere apparition (pointer) du string nee dans le string Hay.
-
-Seulement le nombre Len de characters est cherché
-
-Le loop dois sárreter dans le \0
-
-If : needle[0]=\0 return *Hay
-If trouvé : return &nee[i]
-else  return NULL
-
-
-Cases to handle are:
-
-char *s1 = "see FF your FF return FF now FF";
-           Nee char    *s2 = "FF";
-            size_t  max = strlen(s1);
-
-pointer to nee trough ft_strnstr: 0x0, the needle is:  (null)
-pointer to nee trough strnstr : 0x0, the needle is:  (null)
-—————————
-char *s1 = "FF";
-            char    *s2 = "see F your F return F now F";
-            size_t  max = strlen(s2);
-
-pointer to nee trough ft_strnstr: 0x0, the needle is:  (null)
-pointer to nee trough strnstr : 0x0, the needle is:  (null)
-———————————
-char *s1 = "MZIRIBMZIRIBMZE123";
-            char    *s2 = "MZIRIBMZE";
-            size_t  max = strlen(s2);
-
-pointer to nee trough ft_strnstr: 0x7ffee44ec866, the function returns:  MZIRIBMZE123
-pointer to nee trough strnstr : 0x0, thefunction returns:  (null)
-
-
-pointer to nee trough ft_strnstr: 0x7ff7b5407620, the needle is:  MZIRIBMZE
-pointer to nee trough strnstr : 0x0, the needle is:  MZIRIBMZE
-——————————————
-char *big = "abcdef";
-            char *little = "abcdefghijklmnop";
-            size_t  max = strlen(big);
-
-pointer to nee trough ft_strnstr: 0x7ffee3d43860, the needle is:  abcdef
-pointer to nee trough strnstr : 0x0, the needle is:  (null)
-*/
