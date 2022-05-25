0a40f0417abd2f398bae3f2d0c145c2c96541249
Laila Hasmi <lhasmi@3-E-7.42heilbronn.de>
2022-05-25 21:59:32 +0200

    strjoinSuccesstripouille

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,31 +6,57 @@
 /*   By: lhasmi <lhasmi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 17:53:51 by lhasmi            #+#    #+#             */
-/*   Updated: 2022/05/24 22:05:02 by lhasmi           ###   ########.fr       */
+/*   Updated: 2022/05/25 21:54:55 by lhasmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 #include<stdio.h>
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*compare (char const *s1, char const *s2)
+{
+	char	*buffer;
+
+	if (s1[0] == '\0' && s2[0] != '\0')
+	{
+		buffer = (char	*)malloc(strlen("") + strlen(s2) + 1);
+		if (!buffer)
+			return (NULL);
+		ft_memcpy(buffer, (char	*)s2, ft_strlen(s2) + 1);
+		return (buffer);
+	}
+	else if (s2[0] == '\0' && s1[0] != '\0')
+	{
+		buffer = (char	*)malloc(strlen("") + strlen(s1) + 1);
+		if (!buffer)
+			return (NULL);
+		ft_memcpy(buffer, (char	*)s1, ft_strlen(s1) + 1);
+		return (buffer);
+	}
+	else if (s1[0] == '\0' && s2[0] == '\0')
+	{
+		buffer = (char	*)malloc(strlen("") + strlen("") + 1);
+		if (!buffer)
+			return (NULL);
+		buffer [0] = '\0';
+		return (buffer);
+	}
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 char	*newstr;
 size_t	size;
-// char*	str1;
-// char*	str2;
 
-// str1 = ((char *)s1);
-// str2 = ((char *)s2);
-// if (str1[0] == '\0')
-// 	return (str2);
-if (s1 == NULL && s2 == NULL)
-	return(NULL);
-size = ft_strlen(s1) + ft_strlen(s2) + 1;
-newstr = malloc(sizeof(char) * size);
-if (!newstr)
-		return(NULL);
-ft_strlcat(newstr, (char *)s1, ft_strlen(s1) + 1);
-ft_strlcat(newstr, (char *)s2, size);
-return(newstr);
+compare (s1, s2);
+if (s2[0] != '\0' && s1[0] != '\0')
+{
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	newstr = malloc(sizeof(char) * size);
+	if (!newstr)
+			return(NULL);
+	ft_memcpy(newstr, (char *)s1, ft_strlen(s1) + 1);
+	ft_strlcat(newstr, (char *)s2, size);
+	return(newstr);
+}
 }
