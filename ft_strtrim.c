08a931488674e1ba0d3cf2534cbc596bb2197807
Laila H <lailah@Lailas-MacBook-Pro.local>
2022-05-31 09:31:27 +0200

    jetrime

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,42 +6,66 @@
 /*   By: lailah <lailah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 12:43:09 by lhasmi            #+#    #+#             */
-/*   Updated: 2022/05/29 13:25:58 by lailah           ###   ########.fr       */
+/*   Updated: 2022/05/31 09:30:48 by lailah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// 1. I will use strnstr to find the set in the string and 
-//	 then if *string == the return value of the strnstr. 
-//	 to say if the first position of the string equals the first position of set
-// 2. because i must verify set given by user is at beginning and the end of s
-// 3. then i must proceed to the mem allocation of the "trimdsubstring"
-// 4. then I give the value to the created memory by 
-// 		substr with the start argument being the *set+ strlen(set)
-// 5. this will extract the substring after the set at the beginning
-// 6. then for the last part....
-// 7.  I use strnstr to extract the part before from the trimdsubstring
-
+#include <string.h>
+#include <stdio.h>
 #include"libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-char	*s1trimed;
-char	*setfound;
-int	setlen;
-int	s1len;
+//char	*s1trimed;
+size_t	setlen;
+size_t	s1len;
+size_t	s1len2;
+size_t	i;
 
 setlen = ft_strlen(set);
 s1len = ft_strlen(s1);
-setfound = ft_strnstr(s1, set, s1len);
-if (setfound)
+printf("%s\n", s1);
+i = 0;
+while (s1 && i < s1len)
 {
-	if(*s1 == *set && *(s1 + (s1len - 1)) == *(set + (setlen - 1)))
+	if (ft_strchr(set, *(s1 + i)))
+		s1++;
+	i++;
+}
+printf(" substring afterfirstloop: %s\n", s1);
+s1len2 = ft_strlen(s1);
+printf(" s1length afterfirstloop: %zu\n", s1len2);
+i = 0;
+while (s1)
+{
+	printf("entersecondloop\n");
+	if (ft_strchr(set, *(s1 + i)) == NULL)
 	{
-		s1trimed = (char	*)malloc((s1len - (setlen * 2)) + 1);
-		if (!s1trimed)
-			return (NULL);
+		i++;
+		printf(" substring inside 1st ifcondition: %s\n", s1);
+		printf(" counter inside 1st ifcondition:: %zu\n", i);
+		s1++;
 	}
-	return (s1trimed);
+	if (ft_strchr(set, *(s1 + i)) != NULL)
+		{
+		printf(" substring inside 2nd ifcondition: %s\n", s1);
+		break;	
+		}
 }
-return(0);
+printf(" substring after2ndloop: %s\n", s1);
+return ((char *)s1);
 }
+// allocate memory after the seconde loop
+int main()
+{
+	char	*s1 = "x   xxxtripouillexx";
+	//char	*s2 = "Hello \t  Please\n Trim me !";
+	char	*set = " x";
+	char	*returnval = ft_strtrim(s1, set);
+
+	printf("Ret: %s\n", returnval);
+	// char buffer2[1000] = "there is no stars in the sky";
+	// size_t ret2 = ft_strlcat(buffer2, str, strlen(str) + 4);
+	// printf("Ret: %zu, buffer: %s\n", ret2, buffer2);
+}
+
